0385f8bcfcd710ca49bdd022250836f3f9dab50d
aarce104 <aarce104@gmail.com>
2017-06-28 00:19:03 -0400

    Added connection BitstampUtils, PoloniexUtils classes and PusherClientGeneric and WAMPClientGeneric interfaces.

--- src/main/java/utils/PoloniexUtils.java
+++ src/main/java/utils/PoloniexUtils.java
@@ -1,40 +1,33 @@
-package entry;
+package utils;
 
-import utils.BitstampUtils;
-import java.util.Scanner;
+import ws.wamp.jawampa.*;
+import ws.wamp.jawampa.connection.IWampConnectorProvider;
+import ws.wamp.jawampa.transport.netty.*;
+import rx.functions.Action1;
+import java.util.concurrent.TimeUnit;
 
 /**
- * Created by Abraham on 6/10/17.
+ * Created by Abraham on 6/27/17.
  */
-public class CrittoMercatoMain {
-    public static void main(String[] args) throws Exception {
-        String apiKey, channelName, eventName;
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter Bitstamp apiKey ");
-        apiKey = in.next();
-        System.out.println("Enter Bitstamp channelName ");
-        channelName = in.next();
-        System.out.println("Enter Bitstamp eventName");
-        eventName = in.next();
 
-       new BitstampUtils().getPusherClient(apiKey, channelName, eventName);
+public class PoloniexUtils implements WAMPClientGeneric{
+    public PoloniexUtils(){
 
-        //while (true) {
-            //int i = 1;
-        //}
+    }
 
-        //Thread.sleep(100000);
-        /*WampClient client;
+    public void getWAMPClient(String URI, String Realm, String feed){
+        WampClient client;
         try{
             WampClientBuilder builder = new WampClientBuilder();
             IWampConnectorProvider connectorProvider = new NettyWampClientConnectorProvider();
             builder.withConnectorProvider(connectorProvider)
-                    .withUri("wss://api.poloniex.com")
-                    .withRealm("realm1")
+                    .withUri(URI)
+                    .withRealm(Realm)
                     .withInfiniteReconnects()
                     .withReconnectInterval(5, TimeUnit.SECONDS);
             client = builder.build();
-        } catch (Exception e){
+
+        }catch (Exception e){
             return;
         }
         client.statusChanged().subscribe(new Action1<WampClient.State>() {
@@ -43,18 +36,20 @@ public class CrittoMercatoMain {
                 System.out.println("Session status changed to " + t1);
             }
         });
+
         client.statusChanged().subscribe(new Action1<WampClient.State>() {
             @Override
             public void call(WampClient.State t1) {
                 if (t1 instanceof WampClient.ConnectedState) {
-                    client.makeSubscription("ticker")
+                    client.makeSubscription(feed)
                             .subscribe((s) -> {
                                 System.out.println(s.arguments()); });
                 } System.out.println(t1);
             }
         });
+
         client.open();
-*/
 
     }
+
 }
